00f9381d5ee90f8fc942695c652f4a749f7a31f2
xuyonglu <xuyonglu@zimiker.com>
2015-11-19 13:24:44 +0800

    加入0V offset

--- adc.c
+++ adc.c
@@ -8,10 +8,24 @@ u16 getAdcValue(u8 channel)
 {
 	
 	u16 temp = 0xffff;
-	u8 tempH,tempL;
+	
+	short value; 
+	char  rAdc_Offset;
 
 
-	ADCCRL &= 0xF0;
+	// read adc offset
+#pragma asm
+	mov A, #0			;
+	mov DPTR, #3FFFh	;
+	movc A, @A+DPTR		;
+#pragma endasm
+
+	rAdc_Offset = ACC;		//; offset value
+
+
+	ADCCRH = 0x07;
+	ADCCRL = 0x80;           	         // VDD reference
+	
 	ADCCRL |= channel;
 
 	ADCCRL |= (1<<6);
@@ -24,17 +38,26 @@ u16 getAdcValue(u8 channel)
 			break;
 	}
 
-	tempL = ADCDRL;	
-	tempH = ADCDRH;
-	
 
-	
-	temp = ((u16)tempH)<<8;
-	temp += tempL;
+	value = (ADCDR & 0xfff);	
 
-	ADCCRL &= (~(1<<6));
+	// adjust adc value
+	if(rAdc_Offset >= -3 && rAdc_Offset <= 7)
+	{
+	//	rAdc_Offset += 2;
+		if(value > rAdc_Offset)
+		{
+			value -= rAdc_Offset;
+		}
+		else
+		{
+			value = 0;
+		}
+	}	
+
+	ADCCRL = 0x80;
 	
-	return temp;
+	return value;
 }
 
 u16 getAverage(u8 channel)
